831c2451da96a05b3aac6935cd8ab95a3967bac2
Utsav <utsavbajra619@gmail.com>
2024-04-08 01:41:51 -0400

    added slider in cars image and loaded description from json file

--- scripts/cardetail.js
+++ scripts/cardetail.js
@@ -1,3 +1,4 @@
+//product images slider
 document.addEventListener('DOMContentLoaded', function() {
     const imgItems = document.querySelectorAll('.img-select .img-item');
     const showcase = document.querySelector('.img-showcase');
@@ -10,116 +11,107 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-
-$(document).ready(function(){
-    // With JQuery
+const carData = 
     
-    fetchData(carData);
-    // Fetch all data from json file
-    function fetchData(data) {
-        const dataContainer = document.getElementById('data-container');
-        dataContainer.innerHTML = '';
-        var dataHtml = "";
-        data.forEach((item, index) => {
-            dataHtml += `<!-- list-group-item -->
-            <div class="list-group-item car-listing-item container-fluid no-h-padding">
-                <div class="car-row">
-                    <div class="display-car-style">
-                        
-                        <!-- car-listing-item-info -->
-                        <div class="car-listing-item-info left column-12">
-                            <div class="car-row">
-                                <div class="column-small-7">
-                                    <div class="car-listing-item-title">
-                                        <a href="cardetail.html?index=${index}">${item.Year} ${item.Name} ${item.Trim}</a>
-                                    </div>
-                                    <div class="car-listing-item-field-container">
-                                        <div class="car-listing-item-field car-field-drivetrain ">
-                                            <span class="car-listing-item-field-label">Drivetrain</span>
-                                            <span class="car-listing-item-field-value">${item.Drivetrain}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-transmission ">
-                                            <span class="car-listing-item-field-label">Transmission</span>
-                                            <span class="car-listing-item-field-value">${item.Transmission}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-engine ">
-                                            <span class="car-listing-item-field-label">Engine</span>
-                                            <span class="car-listing-item-field-value">${item.Engine}</span>
-                                        </div>
-                                            <div class="car-listing-item-field car-field-mileage ">
-                                            <span class="car-listing-item-field-label">Mileage</span>
-                                            <span class="car-listing-item-field-value">${item.Mileage}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-door ">
-                                            <span class="car-listing-item-field-label">Doors</span>
-                                            <span class="car-listing-item-field-value">${item.Doors}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-vin ">
-                                            <span class="car-listing-item-field-label">VIN</span>
-                                            <span class="car-listing-item-field-value">${item.VIN}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-stock-number ">
-                                            <span class="car-listing-item-field-label">Stock Number</span>
-                                            <span class="car-listing-item-field-value">${item.Stock_Number}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-transmission-details ">
-                                            <span class="car-listing-item-field-label">Transmission Detail</span>
-                                            <span class="car-listing-item-field-value">${item.Transmission_Detail}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-mpg ">
-                                            <span class="car-listing-item-field-label">MPG (city/hwy)</span>
-                                            <span class="car-listing-item-field-value">${item.MPG}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-bodytype ">
-                                            <span class="car-listing-item-field-label">Body Type</span>
-                                            <span class="car-listing-item-field-value">${item.Body_Type}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-trim ">
-                                            <span class="car-listing-item-field-label">Trim</span>
-                                            <span class="car-listing-item-field-value">${item.Trim}</span>
-                                        </div>
-                                        <div class="car-listing-item-field car-field-fueltype ">
-                                            <span class="car-listing-item-field-label">Fuel Type</span>
-                                            <span class="car-listing-item-field-value">${item.Fuel_Type}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- car-price -->
-                                <div class="car-price-wrapper">
-                                    <div class="pull-right car-price">
-                                        <span class="car-price-value">${(parseFloat(item.Price)).toLocaleString('en-US', {
-                                            style: 'currency',
-                                            currency: 'USD',
-                                            minimumFractionDigits: 2,
-                                            maximumFractionDigits: 2
-                                        })}</span>
-                                    </div>
-                                </div>
-                                <!-- car-price -->
-                            </div>
-                        </div>
-                        <!-- car-listing-item-info ends -->
-                    </div>
-                </div>
-                
-                <!-- button row -->
-                <div class="car-row">
-                    <div class="car-listing-item-buttons column-12">
-                        <button type="button" class="btn btn-apply-online">APPLY ONLINE</button>
-                        <button type="button" class="btn btn-check-availability">CHECK AVAILABILITY</button>
-                        <button type="button" class="btn btn-test-drive">TEST DRIVE</button>
-                    </div>
+    {
+        "Name": "Audi A4",
+        "Model": "A4",
+        "Year": 2005,
+        "Mileage": "20000",
+        "Price": "10000",
+        "Drivetrain": "FWD",
+        "Transmission": "Automatic",
+        "Transmission_Detail": "6-Speed Automatic with Tiptronic",
+        "Engine": "2.0L I4",
+        "Doors": "4",
+        "VIN": "1234567890",
+        "Stock_Number": "A12345",
+        "MPG": "21/29",
+        "Body_Type": "Sedan",
+        "Trim": "Base",
+        "Fuel_Type": "Gasoline",
+        "image_1": "https://s.aolcdn.com/commerce/autodata/images/USB50AUC013B0101.jpg",
+        "image_2": "https://s.aolcdn.com/commerce/autodata/images/USB50AUC013B0102.jpg",
+        "image_3": "https://s.aolcdn.com/commerce/autodata/images/USB50AUC013B0104.jpg",
+        "image_4": "https://s.aolcdn.com/commerce/autodata/images/USB50AUC013B0109.jpg",
+        "image_5": "https://s.aolcdn.com/commerce/autodata/images/USB50AUC013B0112.jpg"
+    
+     
+};
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const outputDiv = document.getElementById('data-container');
+    outputDiv.innerHTML = `
+    
+    <h2 class = "product-title">${carData.Name}</h2>
+      
+            <div class = "product-price">
+              <p class = "new-price">Price: <span>$${carData.Price}</span></p>
+            </div>
+
+
+            <div class="car-listing-item-field-container">
+            <div class="car-listing-item-field car-field-drivetrain ">
+                <span class="car-listing-item-field-label">Drivetrain</span>
+                <span class="car-listing-item-field-value">${carData.Drivetrain}</span>
+            </div>
+            <div class="car-listing-item-field car-field-transmission ">
+                <span class="car-listing-item-field-label">Transmission</span>
+                <span class="car-listing-item-field-value">${carData.Transmission}</span>
+            </div>
+            <div class="car-listing-item-field car-field-engine ">
+                <span class="car-listing-item-field-label">Engine</span>
+                <span class="car-listing-item-field-value">${carData.Engine}</span>
+            </div>
+                <div class="car-listing-item-field car-field-mileage ">
+                <span class="car-listing-item-field-label">Mileage</span>
+                <span class="car-listing-item-field-value">${carData.Mileage}</span>
+            </div>
+            <div class="car-listing-item-field car-field-door ">
+                <span class="car-listing-item-field-label">Doors</span>
+                <span class="car-listing-item-field-value">${carData.Doors}</span>
+            </div>
+            <div class="car-listing-item-field car-field-vin ">
+                <span class="car-listing-item-field-label">VIN</span>
+                <span class="car-listing-item-field-value">${carData.VIN}</span>
+            </div>
+            <div class="car-listing-item-field car-field-stock-number ">
+                <span class="car-listing-item-field-label">Stock Number</span>
+                <span class="car-listing-item-field-value">${carData.Stock_Number}</span>
+            </div>
+            <div class="car-listing-item-field car-field-transmission-details ">
+                <span class="car-listing-item-field-label">Transmission Detail</span>
+                <span class="car-listing-item-field-value">${carData.Transmission_Detail}</span>
+            </div>
+            <div class="car-listing-item-field car-field-mpg ">
+                <span class="car-listing-item-field-label">MPG (city/hwy)</span>
+                <span class="car-listing-item-field-value">${carData.MPG}</span>
+            </div>
+            <div class="car-listing-item-field car-field-bodytype ">
+                <span class="car-listing-item-field-label">Body Type</span>
+                <span class="car-listing-item-field-value">${carData.Body_Type}</span>
+            </div>
+            <div class="car-listing-item-field car-field-trim ">
+                <span class="car-listing-item-field-label">Trim</span>
+                <span class="car-listing-item-field-value">${carData.Trim}</span>
+            </div>
+            <div class="car-listing-item-field car-field-fueltype ">
+                <span class="car-listing-item-field-label">Fuel Type</span>
+                <span class="car-listing-item-field-value">${carData.Fuel_Type}</span>
+            </div>
+        </div>
+      
+
+            <!-- button row -->
+            <div class="car-row">
+                <div class="car-listing-item-buttons column-12">
+                    <button type="button" class="btn btn-apply-online">APPLY ONLINE</button>
+                    <button type="button" class="btn btn-check-availability">CHECK AVAILABILITY</button>
+                    <button type="button" class="btn btn-test-drive">TEST DRIVE</button>
                 </div>
             </div>
-            <!-- list-group-item ends-->
-            `;
-        });
-        dataContainer.innerHTML = dataHtml;
-    }
-
+    `;
 });
-
-
-
 
 
